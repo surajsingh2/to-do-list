335a8854906885d797f7cbe550a9e08de538e063
Suraj Singh <mesuraj30@gmail.com>
2020-05-27 16:10:01 +0530

    final

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AddTodo  v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AddTodo from './components/addTodo';
+import Todos from './components/todos';
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+    AddTodo
+  },
+  created() {
+    this.gettodos();
+  },
+  data() {
+    return {
+      todos:[ ]
+    }
+  },
+  methods : {
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+       .then(this.todos = this.todos.filter(todo => todo.id !== id))
+       .catch(err => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed} = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+       .then(res => this.todos = [...this.todos, res.data])
+       .catch(err => console.log(err));
+    },
+    gettodos() {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        .then(res => this.todos = res.data)
+        .catch(err => console.log(err));
+    }
   }
 }
 </script>
@@ -23,6 +55,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+
 </style>
---
+++ src/components/addTodo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add Todo.." autocomplete="off">
+            <input type="submit" value="submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                title: this.title,
+                completed: false
+            }
+            //Send up to Parent
+            this.$emit('add-todo',newTodo);
+
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style  scoped>
+   form {
+       display: flex;
+   }
+
+   input[type="text"] {
+       flex: 9;
+       padding: 5px;
+   }
+
+   input[type="submit"] {
+       flex:3;
+   }
+</style>
